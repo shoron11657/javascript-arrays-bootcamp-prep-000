1b610d5aec858ef05f10dd5d77eb5d277ec0f0a1
Kevin M <shoron@gmail.com>
2017-09-22 07:04:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,5 @@ function addElementToBeginningOfArray(array,element){return [element,...array]}
 function destructivelyAddElementToBeginningOfArray(array,element){  array.unshift(element)
   return array}
 function addElementToEndOfArray(array,element){return [...array, element]}
+function destructivelyAddElementToEndOfArray(array, element){  array.push(element)
+  return array}
