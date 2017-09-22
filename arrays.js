7593845726d2679558a03a4e7bf64093a16435b0
Kevin M <shoron@gmail.com>
2017-09-22 07:10:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,6 @@ function addElementToBeginningOfArray(array,element){return [element,...array]}
 function destructivelyAddElementToBeginningOfArray(array,element){  array.unshift(element)
   return array}
 function addElementToEndOfArray(array,element){return [...array, element]}
-function destructivelyAddElementToEndOfArray(array, element){  array.push(element)  
+function destructivelyAddElementToEndOfArray(array, element){  array.push(element)
   return array}
+  function accessElementInArray(array,element){return array[0]}
